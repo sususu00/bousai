b0904cd6ce98579e721b5dd3c240884fd958c816
suguru <168205230+sususu00@users.noreply.github.com>
2025-02-10 11:56:44 +0900

    Wording correction

--- php/home.php
+++ php/home.php
@@ -14,7 +14,7 @@ END;
     <div id="home_btns">
         <button onclick="location.href='backpack.php'">防災グッズリスト</button>
         <br>
-        <button onclick="location.href='reg-change-input.php'">会員登録情報確認・更新</button>
+        <button onclick="location.href='reg-change-input.php'">会員登録情報確認・変更</button>
         <br>
         <button onclick="location.href='logout-input.php'">ログアウト</button>
     </div>
